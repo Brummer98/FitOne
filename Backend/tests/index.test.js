3e6edca03c70b93662a60075e4587eb890b72deb
Brummer98 <437019@student.fontys.nl>
2022-12-14 13:05:24 +0100

    Node

---
+++ Backend/tests/index.test.js
@@ -0,0 +1,7 @@
+import index from '../index';
+import React, { useEffect, useState } from "react";
+import { render, screen } from '@testing-library/react';
+
+test('Render the port on index', () => {
+    render(<index />);
+  });
