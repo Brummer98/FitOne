bce1389b7039536de2a113b79af57ef9824b7f26
Brummer98 <437019@student.fontys.nl>
2022-12-22 15:45:45 +0100

    Test from start with Sum()

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -1,30 +1,30 @@
 // React imports
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 // Page, component and functional imports
-import index from '../index';
+// import index from '../index';
 import sum from './sum';
 
 // Mongoose db imports
-import express from 'express';
-import mongoose from 'mongoose';
+// import express from 'express';
+// import mongoose from 'mongoose';
 
 // Essential imports
-import bodyparser from 'body-parser';
-import cors from 'cors';
-import routes from './routes/fitoneRoutes';
+// import bodyparser from 'body-parser';
+// import cors from 'cors';
+// import routes from './routes/fitoneRoutes';
 
 // Connect with express on port {4000}
-const app = express();
-const PORT = 4000; // Frontend runs on Port 3000
+// const app = express();
+// const PORT = 4000; // Frontend runs on Port 3000
 
 // mongo connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/fitoneDB',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/fitoneDB',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
