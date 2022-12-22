bc2ae3fe60b552db36436f41d8d5c15ebcbd0480
Brummer98 <437019@student.fontys.nl>
2022-12-22 15:34:41 +0100

    Test without imports

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -1,51 +1,12 @@
 import sum from './sum';
-// import mongoose from 'mongoose';
-import App from '../../Frontend/src/App';
-import { UserSchema } from '../models/userModel';
-import { 
-  addNewUser,
-  getUsers,
-  getUserWithID,
-  updateUser,
-  deleteUserByID
-} from '../controllers/userControllers';
-import { connection } from 'mongoose';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import index from '../index';
-
-// Create user
-const User = mongoose.model('User', UserSchema);
-
-// beforeAll(async () => {
-//   server = await App.listen(4000);
-//   connection = await MongoClient.connect(
-//     "http://localhost:4000/users",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   ); 
-//   let testUser = new User(
-//     username = "Test",
-//     password = "Password",
-//     calories = 0,
-//     weight = 0,
-//     age = 24
-//   );
-// })
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-// test('adds 2 + 2 to equal 4', () => {
-//   expect(sum(2, 2)).toBe(4);
-// });
-
-// test('test to be created a new user called Test. (200)', () => {
-//   expect(addNewUser(POST, testUser).statusCode.toBe(200));
-// });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
