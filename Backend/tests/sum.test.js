ec2f472d5d2727f9c5e6eeb6fe0361e287b2b008
Brummer98 <437019@student.fontys.nl>
2022-12-29 15:22:28 +0100

    Test new test

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -1,5 +1,12 @@
 import sum from './sum';
+import index from '../index';
+import React, { useEffect, useState } from "react";
+import { render, screen } from '@testing-library/react';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test('Render the port on index', () => {
+    render(<index />);
+  });
