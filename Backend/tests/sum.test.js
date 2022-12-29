23faab99643fcc5f689033e73af9cd68ae21ba29
Brummer98 <437019@student.fontys.nl>
2022-12-29 19:18:33 +0100

    New test with mongodb

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -1,36 +1,28 @@
-import sum from './sum';
-var util= require('util');
-var encoder = new util.TextEncoder('utf-8');
-const {TextDecoder, TextEncoder} = require("util");
-const { MongoClient } = require('mongodb');
+const {MongoClient} = require('mongodb');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+describe('insert', () => {
+  let connection;
+  let db;
 
-// Database Name
-const dbName = 'fitoneDB';
+  beforeAll(async () => {
+    connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db(globalThis.__MONGO_DB_NAME__);
+  });
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
+  afterAll(async () => {
+    await connection.close();
+  });
 
-  // the following code examples can be pasted here...
-  return 'done.';
-}
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
-
-// test('Render the port on index', () => {
-//     render(<index />);
-//   });
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
