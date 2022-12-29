1717d3d4bdeb1006e981275ce5326e6f38b6c9db
Brummer98 <437019@student.fontys.nl>
2022-12-29 16:05:44 +0100

    mongodb > mongoose

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -2,7 +2,7 @@ import sum from './sum';
 var util= require('util');
 var encoder = new util.TextEncoder('utf-8');
 const {TextDecoder, TextEncoder} = require("util");
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongoose');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
