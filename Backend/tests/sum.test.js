668c4f91ed66ac685bc8329a96bf2e3ba8e6d95c
Brummer98 <437019@student.fontys.nl>
2022-12-29 15:46:29 +0100

    Update sum.test.js

--- Backend/tests/sum.test.js
+++ Backend/tests/sum.test.js
@@ -1,7 +1,7 @@
 import sum from './sum';
-import index from '../index';
-import React, { useEffect, useState } from "react";
-import { render, screen } from '@testing-library/react';
+// import index from '../index';
+// import React, { useEffect, useState } from "react";
+// import { render, screen } from '@testing-library/react';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
