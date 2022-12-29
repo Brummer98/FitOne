e74f5b165dbbd097da3d0735d5a7feda7fc62994
Brummer98 <437019@student.fontys.nl>
2022-12-29 19:33:06 +0100

    Update test.js

--- Backend/tests/test.js
+++ Backend/tests/test.js
@@ -19,10 +19,10 @@ describe('insert', () => {
   it('should insert a doc into collection', async () => {
     const users = db.collection('users');
 
-    const mockUser = {_id: 'some-user-id', name: 'John'};
+    const mockUser = {_id: '63a16bd3810dfbe80729ef31', userName: 'Test', password: 'password', calories: 3000, weight: 80, age: 25};
     await users.insertOne(mockUser);
 
-    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    const insertedUser = await users.findOne({_id: '63a16bd3810dfbe80729ef31'});
     expect(insertedUser).toEqual(mockUser);
   });
 });
