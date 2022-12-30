04213d1fcf96403f9ac3d5fe587ef164691ae99c
Brummer98 <437019@student.fontys.nl>
2022-12-30 15:28:40 +0100

    Remove afterEach method in test

--- Backend/tests/userController.test.js
+++ Backend/tests/userController.test.js
@@ -7,7 +7,7 @@ const db = require('./db')
 
 beforeAll(async () => await db.connect())
 
-afterEach(async () => await db.clearDatabase())
+// afterEach(async () => await db.clearDatabase())
 
 afterAll(async () => await db.closeDatabase())
 
