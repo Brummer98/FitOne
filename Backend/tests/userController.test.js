b09f6fda94671a14ab6dc415494af93cebe2b755
Brummer98 <437019@student.fontys.nl>
2023-01-03 16:19:13 +0100

    New things

--- Backend/tests/userController.test.js
+++ Backend/tests/userController.test.js
@@ -1,25 +1,25 @@
-// Import functions backend CRUD
-const userController = require('../controllers/userControllers')
-// Import model
-const User = require('../models/userModel');
-// Import DB file
-const db = require('./db')
+// // Import functions backend CRUD
+// const userController = require('../controllers/userControllers')
+// // Import model
+// const User = require('../models/userModel');
+// // Import DB file
+// const db = require('./db')
 
-beforeAll(async () => await db.connect())
+// beforeAll(async () => await db.connect())
 
-// afterEach(async () => await db.clearDatabase())
+// // afterEach(async () => await db.clearDatabase())
 
-afterAll(async () => await db.closeDatabase())
+// afterAll(async () => await db.closeDatabase())
 
-// Create a describe function
-describe('User found by id', () => {
-    // It...
-    it('User found by id', async done => {
-        const userID = '63a16bd3810dfbe80729ef31'
-        const user = await User.findById(userID)        
+// // Create a describe function
+// describe('User found by id', () => {
+//     // It...
+//     it('User found by id', async done => {
+//         const userID = '63a16bd3810dfbe80729ef31'
+//         const user = await User.findById(userID)        
 
-        // Check the userName
-        expect(user.userName).toEqual("BramvHout")
-        done()
-    })
-})
+//         // Check the userName
+//         expect(user.userName).toEqual("BramvHout")
+//         done()
+//     })
+// })
