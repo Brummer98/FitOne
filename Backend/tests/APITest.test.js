6c6c7b426e09999e9c7c509c417d184ac2408b29
Brummer98 <437019@student.fontys.nl>
2023-01-09 13:56:47 +0100

    Update APITest.test.js

--- Backend/tests/APITest.test.js
+++ Backend/tests/APITest.test.js
@@ -35,14 +35,15 @@ describe("Testing backend CRUD", () => {
 
   // getUsers()
   it("should return all users", async () => {
+    jest.setTimeout(30000);
     const res = await Request(app).get("/users");
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
-    jest.setTimeout(30000);
   });
 
   // addNewUser()
   it("should create a user", async () => {
+    jest.setTimeout(30000);
     const res = await Request(app).post("/users").send({
       userName: "Test",
       password: "Pass",
@@ -50,18 +51,18 @@ describe("Testing backend CRUD", () => {
     expect(res.statusCode).toBe(200);
     expect(res.body.userName).toBe("Test");
     elementID = res.body._id;
-    jest.setTimeout(30000);
   });
 
   // getUserWithID()
   it("should return a specific user", async () => {
+    jest.setTimeout(30000);
     const res = await Request(app).get(`/user/${elementID}`);
     expect(res.statusCode).toBe(200);
-    jest.setTimeout(30000);
   });
 
   // updateUser()
   it("should update a product", async () => {
+    jest.setTimeout(30000);
     const res = await Request(app)
       .put(`/user/${elementID}`)
       .send({
@@ -70,16 +71,15 @@ describe("Testing backend CRUD", () => {
       });
     expect(res.statusCode).toBe(200);
     expect(res.body.userName).toBe("UpdatedUsername");
-    jest.setTimeout(30000);
   });
 
   // deleteUserByID()
   it("should delete a user", async () => {
+    jest.setTimeout(30000);
     const res = await Request(app).delete(
       `/user/${elementID}`
     );
     expect(res.statusCode).toBe(200);
-    jest.setTimeout(30000);
   });
 
 });
