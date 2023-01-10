8fc09d9af86c99d5a8828f7514cc6ea2c4462d30
Brummer98 <437019@student.fontys.nl>
2023-01-10 16:22:07 +0100

    Frontend stuff

--- Frontend/tests/index.test.js
+++
@@ -1,7 +0,0 @@
-import index from '../../Backend/index';
-import React, { useEffect, useState } from "react";
-import { render, screen } from '@testing-library/react';
-
-test('Render the port on index', () => {
-    render(<index />);
-  });
