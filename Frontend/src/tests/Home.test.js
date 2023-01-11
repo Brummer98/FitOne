5d64c6ace44d54181499e899694088471bbd1531
Brummer98 <437019@student.fontys.nl>
2023-01-11 10:23:39 +0100

    Final things

---
+++ Frontend/src/tests/Home.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import Navbar2 from "../components/Navbar2.0";
+
+test('renders the landing page', () => {
+    render(<Navbar2 />);
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
